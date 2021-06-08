824236863cad44e7ff91ba2ee990b7d292df77dd
Esteban L. Castro-Feliciano <ecastro@crown-hydro.com>
2021-06-08 18:35:21 -0400

    added napoleon settings

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,10 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Napoleon settings
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+
 
 # -- Options for HTML output -------------------------------------------------
 
