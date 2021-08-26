32e3ce6acff40b794ee1c28bdc184c28121092d4
Esteban L. Castro-Feliciano <ecastro@crown-hydro.com>
2021-08-26 17:28:03 -0400

    version bump

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Esteban L. Castro-Feliciano'
 author = 'Esteban L. Castro-Feliciano'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="openplaning",
-    version="0.1.0",
+    version="0.1.1",
     author="Esteban L. Castro-Feliciano",
     author_email="ecastro@crown-hydro.com",
     description="Hydrodynamic evaluation of planing hulls based on the Savitsky empirical methods.",
